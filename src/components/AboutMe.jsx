af8878c646693a8ec94961d95b7e1f016c798c5f
ABL <zbyrd72@gmail.com>
2024-01-27 12:21:41 -0500

    updated about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,20 +3,22 @@ const AboutMe = () => {
     <div className="aboutMe">
       <h3>About Me</h3>
       <p>
-        I'm an aspiring software engineer with a background in the medical
-        field. I hold a Master's Degree in Medical Science and have worked as a
-        physician assistant treating patients in the emergency department and
-        urgent care. My journey into technology began a few years ago when I
-        discovered my passion for coding. This newfound interest soon
-        transformed into a determination to pursue a career in software
-        engineering. Driven by this passion, I've dedicated a significant
-        portion of my time to programming. I've recently completed a
-        comprehensive full stack software engineering bootcamp at Coding Temple.
-        While I may be new to the professional world of software engineering, I
-        compensate with an unyielding desire to learn and improve. My previous
-        career as a Physician Assistant honed valuable skills that will
-        crossover to the tech world: strong communication, teamwork, and the
-        confidence to tackle challenging problems head-on.
+        I'm a software engineer with a background in the medical field. I hold a
+        Master's Degree in Medical Science, and prior to transitioning to tech I
+        worked as a physician assistant in the emergency department and urgent
+        care settings. In many ways I've always been a tech geek, but my journey
+        into software engineering began a few years ago when I discovered my
+        passion for coding. I'm a graduate of Coding Temple where I completed a
+        comprehensive full stack software engineering bootcamp.
+      </p>
+      <p>
+        When I'm not coding I enjoy cooking various cuisines, being outside, and
+        recently, watching 90's movies.
+      </p>
+      <p>
+        I'm currently looking for a full time software engineering position so
+        please browse my portfolio and reach out if you think I would be a fit
+        for your company.
       </p>
     </div>
   );
